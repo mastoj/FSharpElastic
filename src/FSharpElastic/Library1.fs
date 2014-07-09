7811018f8f72935e2b7e29192e2b705f12e4c0f9
Tomas Jansson <tomas.jansson@gmail.com>
2014-07-09 08:49:56 +0200

    initial commit

---
+++ src/FSharpElastic/Library1.fs
@@ -0,0 +1,5 @@
+﻿namespace FSharpElastic
+
+type Class1() = 
+    member this.X = "F#"
+
---
+++ src/FSharpElastic/Script.fsx
@@ -0,0 +1,58 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open FSharpElastic
+
+// Define your library scripting code here
+
+type Operator =
+    | And
+    | Or
+
+type ZeroTermsQuery = 
+    | All
+
+type MessageOptions = 
+    | Operator of Operator
+    | ZeroTermsQuery of ZeroTermsQuery
+    | CutoffFrequency of double
+
+type Message = 
+    | QueryString of string
+    | Options of query: string * MessageOptions list
+
+type QueryStringOptions = 
+    | DefaultField of string
+    | Query of string
+    | DefaultOperator of Operator
+    | Analyzer of string
+    | AllowLeadingWildCard of bool
+    | LowercaseExpandedTerms of bool
+    | EnablePositionIncrements of bool
+    | FuzzyMaxExpansions of int
+
+type MultiMatchOptions = 
+    | Fields of string list
+
+type BoolOptions = 
+    | MinimumShouldMatch
+    | Boost
+
+type Query =
+    | Match of message: Message
+    | QueryString of options: QueryStringOptions list
+    | MultiMatch of query: string * MultiMatchOptions
+    | Bool of clauses: BoolClause list * options: BoolOptions
+
+and BoolClause = 
+    | Must of query: Query
+    | MustNot of query: Query
+    | Should of queries: Query list
+
+type SearchQuery = 
+    | Query of Query
+
+
+let simpleMatch = Query(Match(Message.QueryString("tomas")))
+let complexMatch = Query(Match(Options("tomas", [Operator(And); ZeroTermsQuery(All); CutoffFrequency(0.001)])))
