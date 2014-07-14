5bfd2ff6817435f1a980f54e45310dbacc352b82
Tomas Jansson <tomas.jansson@gmail.com>
2014-07-14 11:28:04 +0200

    added parsing with FSharp.Data

--- src/FSharpElastic/Library1.fs
+++ src/FSharpElastic/Library1.fs
@@ -3,3 +3,5 @@
 type Class1() = 
     member this.X = "F#"
 
+
+
--- src/FSharpElastic/Script.fsx
+++ src/FSharpElastic/Script.fsx
@@ -1,8 +1,8 @@
-// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
-open FSharpElastic
+#r "../packages/FSharp.Data.2.0.9/lib/net40/FSharp.Data.dll"
+open FSharp.Data
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Quotations.Patterns
 
@@ -28,45 +28,45 @@ type MatchOption =
     | ZeroTermsQuery of ZeroTermsQuery
     | CutoffFrequency of double
 
-type QueryStringOptions = 
-    | DefaultField of string
-    | Query of string
-    | DefaultOperator of Operator
-    | Analyzer of string
-    | AllowLeadingWildCard of bool
-    | LowercaseExpandedTerms of bool
-    | EnablePositionIncrements of bool
-    | FuzzyMaxExpansions of int
+//type QueryStringOptions = 
+//    | DefaultField of string
+//    | Query of string
+//    | DefaultOperator of Operator
+//    | Analyzer of string
+//    | AllowLeadingWildCard of bool
+//    | LowercaseExpandedTerms of bool
+//    | EnablePositionIncrements of bool
+//    | FuzzyMaxExpansions of int
+//
+//type MultiMatchOptions = 
+//    | Fields of string list
 
-type MultiMatchOptions = 
-    | Fields of string list
-
-type BoolOptions = 
-    | MinimumShouldMatch
-    | Boost
+//type BoolOptions = 
+//    | MinimumShouldMatch
+//    | Boost
 
 type Field<'T, 'TR> = Expr<'T -> 'TR> * 'TR
 
 type Fields<'T, 'TR> = Expr<'T -> 'TR> list * 'TR
 
 type SingleField<'T> =
-    | All of query:string
+    | All of string
     | IntField of Field<'T, int>
     | StringField of Field<'T, string>
 
 type Query<'T> =
-    | Match of SingleField<'T> * options: MatchOption list
-    | QueryString of options: QueryStringOptions list
-    | MultiMatch of query: string * MultiMatchOptions
-    | Bool of clauses: BoolClause<'T> list * options: BoolOptions
+    | Match of SingleField<'T> * MatchOption list
+//    | QueryString of options: QueryStringOptions list
+//    | MultiMatch of query: string * MultiMatchOptions
+//    | Bool of clauses: BoolClause<'T> list * options: BoolOptions
 
-and BoolClause<'T> = 
-    | Must of query: Query<'T>
-    | MustNot of query: Query<'T>
-    | Should of queries: Query<'T> list
-
-type SearchQuery<'T> = 
-    | Query of Query<'T>
+//and BoolClause<'T> = 
+//    | Must of query: Query<'T>
+//    | MustNot of query: Query<'T>
+//    | Should of queries: Query<'T> list
+//
+//type SearchQuery<'T> = 
+//    | Query of Query<'T>
 
 type SearchDocument = {PropX: string}
 
@@ -128,6 +128,38 @@ type SingleField<'T> with
         | IntField(f) -> getFieldValue f
         | StringField(f) -> getFieldValue f
 
+
+let operatorToJValue o = 
+    match o with
+    | And -> JsonValue.String("and")
+    | Or -> JsonValue.String("or")
+
+let zeroTermsQueryToJValue (ztq:ZeroTermsQuery) = 
+    match ztq with
+    | ZeroTermsQuery.All -> JsonValue.String("all")
+    | None -> JsonValue.String("none")
+
+let matchOptionToToken option = 
+    match option with
+    | Operator(o) -> ("operator", (operatorToJValue o))
+    | ZeroTermsQuery(ztq) -> ("zero_terms_query", (zeroTermsQueryToJValue ztq))
+
+let matchToToken<'T> ((f:SingleField<'T>), opts) = 
+    match (f, opts) with
+    | (_, []) -> JsonValue.Record([|(f.GetKey, JsonValue.String(f.GetQuery))|])
+    | (_, _) -> 
+        let obj = ("query", JsonValue.String(f.GetQuery))::(opts |> List.map matchOptionToToken) |> List.toArray
+        JsonValue.Record(obj)
+
+let toJToken query = 
+    match query with
+    | Match(f,o) -> JsonValue.Record([|("match", (matchToToken (f,o)))|])
+
+//let queryToJson q = q |> toJToken |> toString
+
+
+
+
 let singleFieldToJson field =
     match field with 
     | All(q) -> keyValueToString "_all" q
@@ -145,14 +177,10 @@ let matchToJson<'T> (field: SingleField<'T>, options: MatchOption list) =
         let allOptionsString = String.concat ", " (query::options)
         sprintf "%s: { %s }" queryKey allOptionsString
 
-let queryToJson query = 
-    match query with
-    | Match (q, options) -> sprintf "{ \"match\" : { %s } }" (matchToJson (q, options))
-
 type Y = {ya: string; yb: int}
 type X = {a: string; b: int; ys: Y list}
 let lambda2 = <@ fun (x:X) -> x.ys.[0].ya @>
 
 //let jsonQuery = queryToJson (Match(StringField(lambda, "tomas"), []))
-let jsonQuery2 = queryToJson (Match(StringField(lambda2, "tomas"), [ZeroTermsQuery(ZeroTermsQuery.All); Operator(And)]))
+//let jsonQuery2 = queryToJson (Match(StringField(lambda2, "tomas"), [ZeroTermsQuery(ZeroTermsQuery.All); Operator(And)]))
     
